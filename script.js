d942dc062b44edc94b53934d5aee5e35bbcd7c1e
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-05-10 12:45:17 +0300

    Added the function that returns minimum item of a matrix

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function printMatrix(matrix) {
     matrix.forEach(row => console.log(row.join(" ")));
 }
+
+function getMinMatrixItem(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    let min = Infinity;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (matrix[i][j] < min) min = matrix[i][j];
+        }
+    }
+
+    return min;
+}
